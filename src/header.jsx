0e58a56d55d4245d524847333c0c01abc52bd728
jdreyespaez <jd.reyespaez@gmail.com>
2016-04-29 21:28:11 +0700

    Handling Form Inputs, using handleClick and handleInputChange to bind the data and send that value to Firebase

--- src/header.jsx
+++ src/header.jsx
@@ -2,14 +2,33 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      text: ''
+    }
+  },
   render: function() {
     return <div className="input-group">
-      <input type="text" className="form-control" />
+      <input
+        value={this.state.text}
+        onChange={this.handleInputChange}
+        type="text"
+        className="form-control" />
       <span className="input-group-btn">
-        <button className="btn btn-default" type="button">
+        <button
+          onClick={this.handleClick}
+          className="btn btn-default"
+          type="button">
           Agregar
         </button>
       </span>
     </div>
+  },
+  handleClick: function() {
+    // Send value of text input to Firebase
+    console.log(this.state.text);
+  },
+  handleInputChange: function(event) {
+    this.setState({text: event.target.value});
   }
 });
