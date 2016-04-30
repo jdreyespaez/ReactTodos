643b9cdc86af7bb063a26cf357edbe9b8dcfc46c
jdreyespaez <jd.reyespaez@gmail.com>
2016-04-30 15:09:31 +0700

    Allowing Edting and Undoing: textChanged was defined as false in getInitialState, in the input of text the event onChange was included, then in the span for group buttons the 'Guardar' and 'Deshacer' btns where included with the changesButtons function. Finally those functions where defined to program the way they are executed in the re-render

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -7,7 +7,8 @@ module.exports = React.createClass({
   getInitialState: function() {
     return {
       text: this.props.item.text,
-      done: this.props.item.done
+      done: this.props.item.done,
+      textChanged: false
     }
   },
   componentWillMount: function() {
@@ -25,17 +26,41 @@ module.exports = React.createClass({
       <input type="text"
         className="form-control"
         value={this.state.text}
+        onChange={this.handleTextChange}
         />
       <span className="input-group-btn">
-        <button className="btn btn-default">
+        {this.changesButtons()}
+        <button
+          className="btn btn-default"
+          onClick={this.handleDeleteClick}
+          >
           Eliminar
         </button>
       </span>
     </div>
   },
+  changesButtons: function() {
+    if(!this.state.textChanged) {
+      return null
+    } else {
+      return <span>
+          <button className="btn btn-default">Guardar</button>
+          <button className="btn btn-default">Deshacer</button>
+        </span>
+    }
+  },
+  handleTextChange: function(event) {
+    this.setState({
+      text: event.target.value,
+      textChanged: true
+    });
+  },
   handleDoneChange: function(event) {
     var update = {done: event.target.checked};
     this.setState(update);
     this.fb.update(update);
+  },
+  handleDeleteClick: function() {
+    this.fb.remove();
   }
 });
